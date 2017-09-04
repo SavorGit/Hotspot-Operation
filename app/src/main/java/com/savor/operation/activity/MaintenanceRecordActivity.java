dbd810b2eabd89ed292c8f6235b9ce94a3a27a0d
bushlee <li.cong@savor.cn>
2017-09-04 21:06:47 +0800

    初始化

--- app/src/main/java/com/savor/operation/activity/MaintenanceRecordActivity.java
+++ app/src/main/java/com/savor/operation/activity/MaintenanceRecordActivity.java
@@ -1,8 +1,42 @@
 package com.savor.operation.activity;
 
+import android.content.Context;
+import android.os.Bundle;
+
 /**
  * Created by bushlee on 2017/9/4.
  */
 
-public class MaintenanceRecordActivity {
+public class MaintenanceRecordActivity extends BaseActivity{
+    private Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+    }
+
+    @Override
+    public void getViews() {
+
+    }
+
+    @Override
+    public void setViews() {
+
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+
+    @Override
+    public void showLoadingLayout() {
+
+    }
+
+    @Override
+    public void hideLoadingLayout() {
+
+    }
 }
