66bfee1c06b37b61b9d8b8f23dd9d8789b941aae
dreamkid <dreamkid0924@163.com>
2017-09-05 17:12:35 +0800

    commit

--- app/src/main/java/com/savor/operation/activity/MainActivity.java
+++ app/src/main/java/com/savor/operation/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.savor.operation.activity;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.savor.operation.R;
@@ -27,6 +29,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private IndexInfoAdapter mAdapter;
     private TextView mVersionTv;
     private TextView mUserNmaeTv;
+    private Button mExitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     public void getViews() {
+        mExitBtn = (Button) findViewById(R.id.btn_exit);
         mUserNmaeTv = (TextView) findViewById(R.id.tv_username);
         mVersionTv = (TextView) findViewById(R.id.tv_version);
         mInfoLv = (RecyclerView) findViewById(R.id.rlv_info);
@@ -75,6 +79,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     public void setListeners() {
+        mExitBtn.setOnClickListener(this);
         mSearchTv.setOnClickListener(this);
         mFixHistoryTv.setOnClickListener(this);
         mExceptionReprotTv.setOnClickListener(this);
@@ -83,6 +88,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn_exit:
+                mSession.setLoginResponse(null);
+                Intent intent = new Intent(this,LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
             case R.id.tv_search:
 
                 break;
