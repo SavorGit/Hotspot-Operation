6650d2e4e55c85a87c61c15d90e63e511b5d65f7
bushlee <li.cong@savor.cn>
2017-09-06 20:07:00 +0800

    初始化

--- app/src/main/java/com/savor/operation/bean/ErrorReport.java
+++ app/src/main/java/com/savor/operation/bean/ErrorReport.java
@@ -11,6 +11,7 @@ public class ErrorReport implements Serializable {
     private static final long serialVersionUID = -1;
 
     private List<ErrorReportBean> list;
+    private int isNextPage;
 
     public List<ErrorReportBean> getList() {
         return list;
@@ -20,10 +21,20 @@ public class ErrorReport implements Serializable {
         this.list = list;
     }
 
+    public int getIsNextPage() {
+        return isNextPage;
+    }
+
+    public void setIsNextPage(int isNextPage) {
+        this.isNextPage = isNextPage;
+    }
+
+
     @Override
     public String toString() {
         return "ErrorReport{" +
                 "list=" + list +
+                ", isNextPage=" + isNextPage +
                 '}';
     }
 }
