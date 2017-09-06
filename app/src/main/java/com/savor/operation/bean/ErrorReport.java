693fd7a7c8d979bd146a241c1a3f06995571a2a7
bushlee <li.cong@savor.cn>
2017-09-06 13:23:03 +0800

    初始化

---
+++ app/src/main/java/com/savor/operation/bean/ErrorReport.java
@@ -0,0 +1,29 @@
+package com.savor.operation.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by admin on 2017/9/6.
+ */
+
+public class ErrorReport implements Serializable {
+    private static final long serialVersionUID = -1;
+
+    private List<ErrorReportBean> list;
+
+    public List<ErrorReportBean> getList() {
+        return list;
+    }
+
+    public void setList(List<ErrorReportBean> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorReport{" +
+                "list=" + list +
+                '}';
+    }
+}
