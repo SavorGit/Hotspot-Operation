47d9002a9a7d2df5f04a1b73417874f6e9ef1091
bushlee <li.cong@savor.cn>
2017-11-09 20:10:19 +0800

    基础类

--- app/src/main/java/com/savor/operation/activity/MissionListActivity.java
+++ app/src/main/java/com/savor/operation/activity/MissionListActivity.java
@@ -2,8 +2,14 @@ package com.savor.operation.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.savor.operation.R;
 
 /**
  * Created by bushlee on 2017/11/5.
@@ -12,9 +18,15 @@ import android.view.View;
 public class MissionListActivity extends AppCompatActivity
         implements View.OnClickListener{
 
+    private Toolbar mToolBar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private PagerSlidingTabStrip mTabLayout;
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotspot_main);
     }
 
     @Override
