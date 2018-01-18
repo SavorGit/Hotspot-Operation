a46e27c7d95bce2e1c3a094bc07c7a9101357ab5
dreamkid <dreamkid0924@163.com>
2018-01-18 17:54:24 +0800

    co

--- app/src/main/java/com/savor/operation/activity/BoxDetailActivity.java
+++ app/src/main/java/com/savor/operation/activity/BoxDetailActivity.java
@@ -1,14 +1,18 @@
 package com.savor.operation.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.savor.operation.R;
+import com.savor.operation.adapter.ProgramStatusAdapter;
 
 public class BoxDetailActivity extends BaseActivity implements View.OnClickListener {
 
     private ImageView mBackBtn;
+    private RecyclerView mProgramRlv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,17 @@ public class BoxDetailActivity extends BaseActivity implements View.OnClickListe
     @Override
     public void getViews() {
         mBackBtn = (ImageView) findViewById(R.id.iv_left);
+        mProgramRlv = (RecyclerView) findViewById(R.id.rlv_program_status);
 
     }
 
     @Override
     public void setViews() {
-
+        ProgramStatusAdapter programStatusAdapter = new ProgramStatusAdapter(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mProgramRlv.setLayoutManager(linearLayoutManager);
+        mProgramRlv.setAdapter(programStatusAdapter);
     }
 
     @Override
